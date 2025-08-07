6a15b2c2f188ee99db6f98732147201725feb00b
khushi_pawar <khushipawar618@gmail.com>
2025-08-08 00:38:42 +0530

    added

--- oops/inheritence.py
+++ oops/inheritence.py
@@ -45,3 +45,52 @@ obj=Manager(1, "Khushi", 30000, 2000, 5, 8000)
 obj1=Developer(2, "Ansh", 40000, 2, 5000)
 obj.calculate_salary()
 obj1.calculate_salary()
+
+
+#hierarchical inhe...**********************************************************************************************
+class Vehicle:
+    def __init__(self,vehicle_no,model,brand):
+        self.__vehicle_no=vehicle_no
+        self.__model=model
+        self.__brand=brand
+        
+    def fuel_type(self):
+        print("this vehicle uses fuel")
+
+class Car(Vehicle):
+    def __init__(self,vehicle_no,brand,boot_space,sunroof):
+        super().__init__(vehicle_no,None,brand)
+        self.boot_space=boot_space
+        self.sunroof=sunroof
+
+    def fuel_type(self):
+        print("car uses Petrol or Diesel")
+        super().fuel_type()
+        
+    
+class Bike(Vehicle):
+    def __init__(self,vehicle_no,brand,engine_cc,car_type):
+        super().__init__(vehicle_no,None,brand)
+        self.engine_cc=engine_cc
+        self.car_type=car_type
+
+    def fuel_type(self):
+        print("Bike uses petrol")
+    
+class Bus(Vehicle):
+    def __init__(self,vehicle_no,brand,seating_capacity,bustype):
+        super().__init__(vehicle_no,None,brand)
+        self.seating_capacity=seating_capacity
+        self.bustype=bustype
+
+    def fuel_type(self):
+        print("bus uses diesel or CNG")
+
+c1 = Car("MP09AB1234", "Hyundai", "450L", True)
+c1.fuel_type()
+
+b1 = Bike("MP09XY6789", "Yamaha", "150cc", "Sports")
+b1.fuel_type()
+
+bu1 = Bus("MP09PK2345", "Volvo", 45, "AC Sleeper")
+bu1.fuel_type()
